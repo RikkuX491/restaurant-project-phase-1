f5f1a0c96ec9a557af6c697e84818a9b5e1afa4f
Ricardo Guerra <RikkuG491@gmail.com>
2024-02-07 11:46:11 -0500

    Added code to display images on website and center the images

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Welcome to my restaurant!')
+const restaurantMenuDivElement = document.getElementById('restaurant-menu')
+
+fetch('http://localhost:3000/foods')
+.then(response => response.json())
+.then(foods => {
+    foods.forEach(food => {
+        const imgElement = document.createElement('img')
+        imgElement.src = food.image
+        restaurantMenuDivElement.appendChild(imgElement)
+    })
+})
